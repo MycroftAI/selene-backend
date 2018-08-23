a6ff57d89199d75d9779b73d79fb51d89a9be872
Chris Veilleux <veilleux.chris@gmail.com>
2018-08-22 22:09:28 -0500

    added setup.py file so that pipenv could install to other local packages

---
+++ shared/setup.py
@@ -0,0 +1,12 @@
+"""
+This is used to support pipenv installing the shared code to the virtual
+environments used in developement of Selene APIs and services.
+"""
+from setuptools import setup
+
+setup(
+    name='selene_util',
+    version='0.0.0',
+    packages=['selene_util'],
+    install_requires=['pygithub', 'pyjwt']
+)
