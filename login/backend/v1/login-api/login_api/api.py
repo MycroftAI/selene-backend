49d00bcc2f2702057383ba86f243f7b4dbcfe510
Chris Veilleux <veilleux.chris@gmail.com>
2018-09-04 19:09:21 -0500

    minor refactoring

--- login/backend/v1/login-api/login_api/api.py
+++ login/backend/v1/login-api/login_api/api.py
@@ -3,10 +3,15 @@ from flask_restful import Api
 
 from .authorize import AuthorizeAntisocialView
 from .config import get_config_location
+from .logout import LogoutView
 
-
+BASE_URL = '/api/auth/'
 login = Flask(__name__)
 login.config.from_object(get_config_location())
-
 login_api = Api(login, catch_all_404s=True)
-login_api.add_resource(AuthorizeAntisocialView, '/api/auth/antisocial')
+
+antisocial_view_url = BASE_URL + 'antisocial'
+login_api.add_resource(AuthorizeAntisocialView, antisocial_view_url)
+
+logout_view_url = BASE_URL + 'logout'
+login_api.add_resource(LogoutView, logout_view_url)
