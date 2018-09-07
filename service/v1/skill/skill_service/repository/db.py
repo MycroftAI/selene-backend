2d7f167804bbb3459f5183cfe2dfbab3d19d1822
Chris Veilleux <veilleux.chris@gmail.com>
2018-09-07 18:45:21 -0500

    use kubernetes DNS instead of environment variables

--- service/v1/skill/skill_service/repository/db.py
+++ service/v1/skill/skill_service/repository/db.py
@@ -5,7 +5,7 @@ from mongoengine import connect
 
 def connect_to_skill_db():
     """Establish a connection to the Mongo skills database."""
-    host = environ['SELENE_SKILL_SERVICE_HOST']
-    port = int(environ['SELENE_SKILL_SERVICE_PORT_SKILL_DB'])
+    host = environ['SKILL_DB_HOST']
+    port = int(environ['SKILL_DB_PORT'])
     database = 'skillDB'
     connect(database, host=host, port=port)
