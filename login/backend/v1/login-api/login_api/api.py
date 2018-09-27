40e11d1bc747d35b3ae807de04222edcf0ee65cf
Matheus Lima <matheuslimaufc@gmail.com>
2018-09-27 16:31:37 -0300

    Fixing the facebook login

--- login/backend/v1/login-api/login_api/api.py
+++ login/backend/v1/login-api/login_api/api.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_restful import Api
 
+from login_api.endpoints.authenticate_social import AuthenticateSocialEndpoint
 from .endpoints import AuthenticateAntisocialEndpoint, LogoutEndpoint
-from .facebook import AuthorizeFacebookView
+from login_api.endpoints.facebook import AuthorizeFacebookView
 from .config import get_config_location
 
 BASE_URL = '/api/auth/'
@@ -12,9 +13,11 @@ login_api = Api(login, catch_all_404s=True)
 
 antisocial_view_url = BASE_URL + 'antisocial'
 facebook_view_url = BASE_URL + 'social/facebook'
+social_view_url = BASE_URL + "social"
 
 login_api.add_resource(AuthenticateAntisocialEndpoint, antisocial_view_url)
 login_api.add_resource(AuthorizeFacebookView, facebook_view_url)
+login_api.add_resource(AuthenticateSocialEndpoint, social_view_url)
 
 logout_view_url = BASE_URL + 'logout'
 login_api.add_resource(LogoutEndpoint, logout_view_url)
--- login/backend/v1/login-api/login_api/endpoints/facebook.py
+++ login/backend/v1/login-api/login_api/endpoints/facebook.py
@@ -1,7 +1,6 @@
 from http import HTTPStatus
 
-import requests as service_request
-from flask import current_app, Response
+from flask import current_app, redirect
 from flask_restful import Resource
 
 THIRTY_DAYS = 2592000
@@ -22,9 +21,7 @@ class AuthorizeFacebookView(Resource):
 
 
     def _validate_token(self):
-        service_request_parameters = {'clientUri': current_app.config['SELENE_BASE_URL']}
-        fb_service_response = service_request.get(
-            current_app.config['TARTARUS_BASE_URL'] + '/social/auth/facebook',
-            params=service_request_parameters
-        )
-        return Response(fb_service_response, mimetype='text/html')
+        tartarus = current_app.config['TARTARUS_BASE_URL']
+        selene = current_app.config['SELENE_BASE_URL']
+        auth_endpoint = f'{tartarus}/social/auth/facebook?clientUri={selene}/api/auth/social&path=/social/login'
+        return redirect(auth_endpoint)
