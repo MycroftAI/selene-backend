c824db4e91b53c70ce9dee87ece07f9ddab771ef
Matheus Lima <matheuslimaufc@gmail.com>
2018-09-27 20:11:17 -0300

    Implemented google authentication

--- login/backend/v1/login-api/login_api/api.py
+++ login/backend/v1/login-api/login_api/api.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 
 from login_api.endpoints.authenticate_social import AuthenticateSocialEndpoint
 from login_api.endpoints.github import AuthorizeGithubView
+from login_api.endpoints.google import AuthorizeGoogleView
 from .endpoints import AuthenticateAntisocialEndpoint, LogoutEndpoint
 from login_api.endpoints.facebook import AuthorizeFacebookView
 from .config import get_config_location
@@ -15,11 +16,13 @@ login_api = Api(login, catch_all_404s=True)
 antisocial_view_url = BASE_URL + 'antisocial'
 facebook_view_url = BASE_URL + 'social/facebook'
 github_view_url = BASE_URL + 'social/github'
+google_view_url = BASE_URL + "social/google"
 social_view_url = BASE_URL + "social"
 
 login_api.add_resource(AuthenticateAntisocialEndpoint, antisocial_view_url)
 login_api.add_resource(AuthorizeFacebookView, facebook_view_url)
 login_api.add_resource(AuthorizeGithubView, github_view_url)
+login_api.add_resource(AuthorizeGoogleView, google_view_url)
 login_api.add_resource(AuthenticateSocialEndpoint, social_view_url)
 
 logout_view_url = BASE_URL + 'logout'
---
+++ login/backend/v1/login-api/login_api/endpoints/google.py
@@ -0,0 +1,14 @@
+from flask import current_app, redirect
+from flask_restful import Resource
+
+
+class AuthorizeGoogleView(Resource):
+
+    def get(self):
+        return self._validate_token()
+
+    def _validate_token(self):
+        tartarus = current_app.config['TARTARUS_BASE_URL']
+        selene = current_app.config['SELENE_BASE_URL']
+        auth_endpoint = f'{tartarus}/social/auth/google?clientUri={selene}/api/auth/social&path=/social/login'
+        return redirect(auth_endpoint)
