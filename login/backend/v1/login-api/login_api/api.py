cab48a64821a427d18c1791320276cbae8a58f0f
Chris Veilleux <veilleux.chris@gmail.com>
2018-09-28 00:11:43 -0500

    some refactoring and added a way to allow other apps to call the login app to logout

--- login/backend/v1/login-api/login_api/api.py
+++ login/backend/v1/login-api/login_api/api.py
@@ -1,29 +1,41 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api
 
-from login_api.endpoints.authenticate_social import AuthenticateSocialEndpoint
-from login_api.endpoints.github import AuthorizeGithubView
-from login_api.endpoints.google import AuthorizeGoogleView
-from .endpoints import AuthenticateAntisocialEndpoint, LogoutEndpoint
-from login_api.endpoints.facebook import AuthorizeFacebookView
+from .endpoints import (
+    AuthenticateAntisocialEndpoint,
+    AuthenticateSocialEndpoint,
+    AuthorizeFacebookView,
+    AuthorizeGithubView,
+    AuthorizeGoogleView,
+    LogoutEndpoint
+)
 from .config import get_config_location
-
-BASE_URL = '/api/auth/'
+# Initialize the Flask application and the Flask Restful API
 login = Flask(__name__)
 login.config.from_object(get_config_location())
 login_api = Api(login, catch_all_404s=True)
 
-antisocial_view_url = BASE_URL + 'antisocial'
-facebook_view_url = BASE_URL + 'social/facebook'
-github_view_url = BASE_URL + 'social/github'
-google_view_url = BASE_URL + "social/google"
-social_view_url = BASE_URL + "social"
+# Define the endpoints
+login_api.add_resource(AuthenticateAntisocialEndpoint, '/api/antisocial')
+login_api.add_resource(AuthorizeFacebookView, '/api/social/facebook')
+login_api.add_resource(AuthorizeGithubView, '/api/social/github')
+login_api.add_resource(AuthorizeGoogleView, '/api/social/google')
+login_api.add_resource(AuthenticateSocialEndpoint, '/api/social')
+login_api.add_resource(LogoutEndpoint, '/api/logout')
 
-login_api.add_resource(AuthenticateAntisocialEndpoint, antisocial_view_url)
-login_api.add_resource(AuthorizeFacebookView, facebook_view_url)
-login_api.add_resource(AuthorizeGithubView, github_view_url)
-login_api.add_resource(AuthorizeGoogleView, google_view_url)
-login_api.add_resource(AuthenticateSocialEndpoint, social_view_url)
 
-logout_view_url = BASE_URL + 'logout'
-login_api.add_resource(LogoutEndpoint, logout_view_url)
+def add_cors_headers(response):
+    """Allow any application to logout"""
+    if 'logout' in request.url:
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        if request.method == 'OPTIONS':
+            response.headers['Access-Control-Allow-Methods'] = (
+                'DELETE, GET, POST, PUT'
+            )
+            headers = request.headers.get('Access-Control-Request-Headers')
+            if headers:
+                response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+
+
+login.after_request(add_cors_headers)
