e39e42f0c6caef310ca20ae2474adc7e5d9655f0
Chris Veilleux <veilleux.chris@gmail.com>
2018-09-28 00:09:18 -0500

    added imports for all endpoints

--- login/backend/v1/login-api/login_api/endpoints/__init__.py
+++ login/backend/v1/login-api/login_api/endpoints/__init__.py
@@ -1,2 +1,6 @@
 from .authenticate_antisocial import AuthenticateAntisocialEndpoint
+from .authenticate_social import AuthenticateSocialEndpoint
+from .facebook import AuthorizeFacebookView
+from .github import AuthorizeGithubView
+from .google import AuthorizeGoogleView
 from .logout import LogoutEndpoint
