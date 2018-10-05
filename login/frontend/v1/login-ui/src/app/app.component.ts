38d7ecafccd95f161a3aa18812b90700e5749f34
Chris Veilleux <veilleux.chris@gmail.com>
2018-10-05 12:33:18 -0500

    simplified directory structure and added logic to handle social logins using the tartarus redirect method.

--- login/frontend/v1/login-ui/src/app/app.component.ts
+++ login/frontend/v1/login-ui/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Mycroft Login';
+    public socialLoginDataFound: boolean = false;
+
+    constructor () {
+    }
+
+    ngOnInit () {
+        let uriParams = decodeURIComponent(window.location.search);
+
+        if (uriParams) {
+            this.socialLoginDataFound = true;
+            window.opener.postMessage(uriParams, window.location.origin);
+            window.close();
+        }
+    }
+
 }
--- login/frontend/v1/login-ui/src/app/auth/auth.service.ts
+++ login/frontend/v1/login-ui/src/app/auth/auth.service.ts
@@ -2,11 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from "@angular/common/http";
 
 import { Observable } from 'rxjs';
+import { isArray } from "util";
 
-export class AuthResponse {
+import { MatSnackBar } from "@angular/material";
+
+export interface AuthResponse {
     expiration: number;
     seleneToken: string;
     tartarusToken: string;
+}
+
+export interface SocialLoginData {
+    uuid: string;
+    accessToken: string;
+    refreshToken: string;
+    expiration: string;
 }
 
 @Injectable()
@@ -15,8 +25,10 @@ export class AuthService {
     private facebookAuthUrl = '/api/social/facebook';
     private githubAuthUrl = '/api/social/github';
     private googleAuthUrl = '/api/social/google';
+    private generateTokensUrl = 'api/social/tokens';
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, public loginSnackbar: MatSnackBar) {
+    }
 
     authorizeAntisocial (username, password): Observable<AuthResponse> {
         let rawCredentials = `${username}:${password}`;
@@ -36,6 +48,60 @@ export class AuthService {
     }
 
     authenticateWithGoogle() {
-        window.open(this.googleAuthUrl)
+        window.open(this.googleAuthUrl);
+        window.onmessage = (event) => {this.generateSocialLoginTokens(event)};
     }
+
+    generateSocialLoginTokens(event: any) {
+        let socialLoginData = this.parseUriParams(event.data);
+        if (socialLoginData) {
+            this.http.post<AuthResponse>(
+                this.generateTokensUrl,
+                socialLoginData
+            ).subscribe(
+                (response) => {this.generateTokenCookies(response)}
+            );
+        }
+        return this.http.post<AuthResponse>(
+            this.generateTokensUrl,
+            socialLoginData
+        )
+    }
+
+    parseUriParams (uriParams: string) {
+        let socialLoginData: SocialLoginData = null;
+
+        if (uriParams.startsWith('?data=')) {
+            let parsedUriParams = JSON.parse(uriParams.slice(6));
+            if (isArray(parsedUriParams)) {
+                let socialLoginErrorMsg = 'An account exists for the email ' +
+                    'address associated with the social network log in ' +
+                    'attempt.  To enable log in using a social network, log ' +
+                    'in with your username and password and enable the ' +
+                    'social network in your account preferences.';
+                this.loginSnackbar.open(
+                    socialLoginErrorMsg,
+                    null,
+                    {duration: 30000}
+                );
+            } else {
+                socialLoginData = <SocialLoginData>parsedUriParams;
+            }
+        }
+
+        return socialLoginData
+    }
+
+    generateTokenCookies(authResponse: AuthResponse) {
+        let expirationDate = new Date(authResponse.expiration * 1000);
+        let domain = document.domain.replace('login.', '');
+        document.cookie = 'seleneToken=' + authResponse.seleneToken +
+            '; expires=' + expirationDate.toUTCString() +
+            '; domain=' + domain;
+        document.cookie = 'tartarusToken=' + authResponse.tartarusToken +
+            '; expires=' + expirationDate.toUTCString() +
+            '; domain=' + domain;
+    }
+
+
 }
