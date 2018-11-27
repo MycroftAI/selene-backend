2dfa71eae13ed3228e7bba4530bd5969dd346bc8
Chris Veilleux <veilleux.chris@gmail.com>
2018-11-27 12:50:08 -0600

    added account application

---
+++ frontend/internet/projects/account/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'account-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'account';
+}
