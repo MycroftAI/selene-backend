a7e091b7fe597abedb7ba140156e8d631e999e71
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-01 00:17:04 -0600

    enhanced and refactored to add crud abilities

--- shared/selene/util/db/__init__.py
+++ shared/selene/util/db/__init__.py
@@ -1,3 +1,3 @@
 from .connection import DatabaseConnectionConfig
 from .connection_pool import allocate_db_connection_pool, get_db_connection
-from .cursor import DatabaseQuery, fetch
+from .cursor import DatabaseRequest, Cursor
