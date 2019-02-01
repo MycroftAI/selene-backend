585f9332db70d25c44d9c3c5ba57781e34a955be
Chris Veilleux <38014245+chrisveilleux@users.noreply.github.com>
2019-02-01 14:03:59 -0600

    Fixed how the packages were identified

--- shared/setup.py
+++ shared/setup.py
@@ -2,11 +2,11 @@
 This is used to support pipenv installing the shared code to the virtual
 environments used in developement of Selene APIs and services.
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='selene',
     version='0.0.0',
-    packages=['selene'],
+    packages=find_packages(),
     install_requires=['flask', 'flask-restful', 'pygithub', 'pyjwt']
 )
