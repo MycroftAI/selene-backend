604fd6108bd105946859f2e5974a3a432370f907
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-02 00:45:35 -0600

    added group by and changed a join to a left join to fix bugs

--- shared/selene/account/repository/sql/get_account_from_credentials.sql
+++ shared/selene/account/repository/sql/get_account_from_credentials.sql
@@ -6,8 +6,11 @@ FROM
     account.account a
 INNER JOIN
     account.account_login al ON al.account_id = a.id
-INNER JOIN
+LEFT JOIN
     account.refresh_token rt ON rt.account_id = a.id
 WHERE
     a.email_address = %(email_address)s AND
     al.token = crypt(%(password)s, al.token)
+GROUP BY
+    a.id,
+    a.email_address
