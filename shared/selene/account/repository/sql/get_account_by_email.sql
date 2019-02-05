41cbf732f1e62f57efe37f848ff2d50402e45027
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-05 12:06:19 -0600

    changed join on refresh token table to outer join in case there are no refresh tokens for an account

--- shared/selene/account/repository/sql/get_account_by_email.sql
+++ shared/selene/account/repository/sql/get_account_by_email.sql
@@ -4,7 +4,7 @@ SELECT
     array_agg(rt.refresh_token) as refresh_tokens
 FROM
     account.account a
-INNER JOIN
+LEFT JOIN
     account.refresh_token rt on a.id = rt.account_id
 WHERE
     a.email_address = %(email_address)s
