6f6cd0aac6d797ee31bd78573336da341034bd65
Matheus Lima <matheuslimaufc@gmail.com>
2019-02-08 16:13:32 -0300

    Using relative imports

--- api/public/api.py
+++ api/public/api.py
@@ -2,15 +2,15 @@ from flask import Flask
 from flask_restful import Api
 from selene.api import JSON_MIMETYPE, output_json
 
-from endpoints.device import DeviceEndpoint
-from endpoints.device_setting import DeviceSettingEndpoint
-from endpoints.device_skill import DeviceSkillEndpoint
-from endpoints.device_skills import DeviceSkillsEndpoint
+from .endpoints.device import DeviceEndpoint
+from .endpoints.device_setting import DeviceSettingEndpoint
+from .endpoints.device_skill import DeviceSkillEndpoint
+from .endpoints.device_skills import DeviceSkillsEndpoint
 from selene.api.base_config import get_base_config
 
-from endpoints.device_subscription import DeviceSubscriptionEndpoint
-from endpoints.open_weather_map import OpenWeatherMapEndpoint
-from endpoints.wolfram_alpha import WolframAlphaEndpoint
+from .endpoints.device_subscription import DeviceSubscriptionEndpoint
+from .endpoints.open_weather_map import OpenWeatherMapEndpoint
+from .endpoints.wolfram_alpha import WolframAlphaEndpoint
 
 public = Flask(__name__)
 public.config.from_object(get_base_config())
@@ -24,5 +24,5 @@ public_api.add_resource(DeviceSkillEndpoint, '/device/<string:device_id>/userSki
 public_api.add_resource(DeviceEndpoint, '/device/<string:device_id>')
 public_api.add_resource(DeviceSettingEndpoint, '/device/<string:device_id>/setting')
 public_api.add_resource(DeviceSubscriptionEndpoint, '/device/<string:device_id>/subscription')
-public_api.add_resource(WolframAlphaEndpoint, '/wa')
-public_api.add_resource(OpenWeatherMapEndpoint, '/owm/<path:path>')
+public_api.add_resource(WolframAlphaEndpoint, '/wa')  # TODO: change this path in the API v2
+public_api.add_resource(OpenWeatherMapEndpoint, '/owm/<path:path>')     # TODO: change this path in the API v2
