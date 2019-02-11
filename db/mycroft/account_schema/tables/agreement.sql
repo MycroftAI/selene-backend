918f24ccf3b1e2d2bf8504aee19c40181d48a833
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-11 16:42:59 -0600

    added an enum to define the types of agreements allowed and changed "content" column to "content_id"

--- db/mycroft/account_schema/tables/agreement.sql
+++ db/mycroft/account_schema/tables/agreement.sql
@@ -1,8 +1,8 @@
 CREATE TABLE account.agreement (
-    id              uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    agreement       text NOT NULL,
-    version         text NOT NULL,
-    effective_dt    date NOT NULL,
-    content         oid  NOT NULL,
+    id              uuid            PRIMARY KEY DEFAULT gen_random_uuid(),
+    agreement       agreement_enum  NOT NULL,
+    version         text            NOT NULL,
+    effective_dt    date            NOT NULL,
+    content_id      oid             NOT NULL,
     UNIQUE (agreement, version)
 );
