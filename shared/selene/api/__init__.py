49d43d061dda57d145b5715d99dd6f929defe021
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-12 11:17:19 -0600

    new agreements endpoint

--- shared/selene/api/__init__.py
+++ shared/selene/api/__init__.py
@@ -1,4 +1,5 @@
 from .account_endpoint import AccountEndpoint
+from .agreements_endpoint import AgreementsEndpoint
 from .base_endpoint import APIError, SeleneEndpoint
 from .base_config import get_base_config
 from .response_data_formatter import JSON_MIMETYPE, output_json
