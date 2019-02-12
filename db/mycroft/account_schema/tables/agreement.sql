662fb1959dfbd2174222abbc8c34622b22c4516c
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-12 10:40:54 -0600

    minor schema changes

--- db/mycroft/account_schema/tables/agreement.sql
+++ db/mycroft/account_schema/tables/agreement.sql
@@ -2,7 +2,8 @@ CREATE TABLE account.agreement (
     id              uuid            PRIMARY KEY DEFAULT gen_random_uuid(),
     agreement       agreement_enum  NOT NULL,
     version         text            NOT NULL,
-    effective_dt    date            NOT NULL,
+    effective       daterange       NOT NULL,
     content_id      oid             NOT NULL,
+    EXCLUDE USING gist (agreement WITH =, effective WITH &&),
     UNIQUE (agreement, version)
 );
