8c2eb61aa1d9a0f61c93765028e36e64a9eb2313
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-15 21:42:44 -0600

    simplified module name and converted from flask-restful to flask

--- shared/selene/api/__init__.py
+++ shared/selene/api/__init__.py
@@ -1,3 +1,3 @@
 from .base_endpoint import APIError, SeleneEndpoint
 from .base_config import get_base_config
-from .response_data_formatter import JSON_MIMETYPE, output_json
+from .response import SeleneResponse
