fda4f7a7888c32e250f9b363e28ca0acb0cb0b80
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-15 21:45:08 -0600

    added test for a missing attribute in the new account request

--- api/account/tests/features/new_account.feature
+++ api/account/tests/features/new_account.feature
@@ -5,3 +5,7 @@ Feature: Add a new account
     When a valid new account request is submitted
     Then the request will be successful
      And the account will be added to the system
+
+  Scenario: Request missing a required field
+    When a request is sent without an email address
+    Then the request will fail with a bad request error
--- api/account/tests/features/steps/new_account.py
+++ api/account/tests/features/steps/new_account.py
@@ -8,24 +8,40 @@ from hamcrest import assert_that, equal_to, is_in, not_none
 from selene.data.account import AccountRepository, PRIVACY_POLICY, TERMS_OF_USE
 from selene.util.db import get_db_connection
 
+new_account_request = dict(
+    displayName='barfoo',
+    termsOfUse=True,
+    privacyPolicy=True,
+    login=dict(
+        federatedEmail=None,
+        userEnteredEmail='bar@mycroft.ai',
+        password='bar'
+    ),
+    support=dict(
+        openDataset=True,
+        membership='Monthly Supporter',
+        stripeCustomerId='barstripe'
+    )
+)
+
 
 @when('a valid new account request is submitted')
 def call_add_account_endpoint(context):
-    context.new_account_request = dict(
-        displayName='barfoo',
-        termsOfUse=True,
-        privacyPolicy=True,
-        login=dict(
-            federatedEmail=None,
-            userEnteredEmail='bar@mycroft.ai',
-            password='bar'
-        ),
-        support=dict(
-            openDataset=True,
-            membership='Monthly Supporter',
-            stripeCustomerId='barstripe'
-        )
+    context.new_account_request = new_account_request
+    context.client.content_type = 'application/json'
+    context.response = context.client.post(
+        '/api/account',
+        data=json.dumps(context.new_account_request),
+        content_type='application_json'
     )
+
+
+@when('a request is sent without an email address')
+def create_account_without_email(context):
+    context.new_account_request = new_account_request
+    login_data = context.new_account_request['login']
+    del(login_data['userEnteredEmail'])
+    context.new_account_request['login'] = login_data
     context.client.content_type = 'application/json'
     context.response = context.client.post(
         '/api/account',
@@ -58,3 +74,8 @@ def check_db_for_account(context):
         for agreement in account.agreements:
             assert_that(agreement.type, is_in((PRIVACY_POLICY, TERMS_OF_USE)))
             assert_that(agreement.accept_date, equal_to(str(date.today())))
+
+
+@then('the request will fail with a bad request error')
+def check_for_bad_request(context):
+    assert_that(context.response.status_code, equal_to(HTTPStatus.BAD_REQUEST))
