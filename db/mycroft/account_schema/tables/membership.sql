ac01371c40fa1d368fce00c2379acb3c838076ae
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-24 23:46:06 -0600

    changed "subscription" to "membership"

--- db/mycroft/account_schema/tables/membership.sql
+++ db/mycroft/account_schema/tables/membership.sql
@@ -1,6 +1,6 @@
-CREATE TABLE account.subscription (
+CREATE TABLE account.membership (
     id              uuid        PRIMARY KEY DEFAULT gen_random_uuid(),
-    subscription    text        NOT NULL UNIQUE,
+    type            text        NOT NULL UNIQUE,
     rate            NUMERIC     NOT NULL,
     rate_period     text        NOT NULL,
     insert_ts       TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
