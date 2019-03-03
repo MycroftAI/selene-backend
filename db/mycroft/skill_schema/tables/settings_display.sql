96649e7ebf44564b82360b8d248f8a6266c8c58c
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-03 14:09:46 -0600

    added hash column back to support older versions of core

--- db/mycroft/skill_schema/tables/settings_display.sql
+++ db/mycroft/skill_schema/tables/settings_display.sql
@@ -3,7 +3,8 @@ CREATE TABLE skill.settings_display (
             DEFAULT gen_random_uuid(),
     skill_id            uuid        NOT NULL
             REFERENCES skill.skill ON DELETE CASCADE,
-    settings_display   jsonb       NOT NULL,
+    settings_display    jsonb       NOT NULL,
+    hash                text        NOT NULL,
     insert_ts           TIMESTAMP   NOT NULL
         DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (skill_id, settings_display)
