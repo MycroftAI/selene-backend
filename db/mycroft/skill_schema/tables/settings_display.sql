6ce6e21acff5eaa3de112bd67c84174f3e5ba03d
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-03 18:03:43 -0600

    removed hash column again because it was determined to be used in a device ownership feature that will be deprecated.

--- db/mycroft/skill_schema/tables/settings_display.sql
+++ db/mycroft/skill_schema/tables/settings_display.sql
@@ -4,7 +4,6 @@ CREATE TABLE skill.settings_display (
     skill_id            uuid        NOT NULL
             REFERENCES skill.skill ON DELETE CASCADE,
     settings_display    jsonb       NOT NULL,
-    hash                text        NOT NULL,
     insert_ts           TIMESTAMP   NOT NULL
         DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (skill_id, settings_display)
