07c7cc70dbad9c386b098eae1b22f57c64bdd61f
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-08 17:02:17 -0600

    added stripe plan name column

--- db/mycroft/account_schema/tables/membership.sql
+++ db/mycroft/account_schema/tables/membership.sql
@@ -1,10 +1,11 @@
 CREATE TABLE account.membership (
     id              uuid                    PRIMARY KEY
             DEFAULT gen_random_uuid(),
-    type            membership_type_enum     NOT NULL
+    type            membership_type_enum    NOT NULL
             UNIQUE,
     rate            NUMERIC                 NOT NULL,
     rate_period     text                    NOT NULL,
+    stripe_plan     text                    NOT NULL,
     insert_ts       TIMESTAMP               NOT NULL
         DEFAULT CURRENT_TIMESTAMP
 );
