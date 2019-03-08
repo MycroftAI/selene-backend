885d36b34ea600402932338e423c2eff1dedc8dd
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-08 17:27:05 -0600

    added stripe_plan to the insert statements

--- db/mycroft/account_schema/data/membership.sql
+++ db/mycroft/account_schema/data/membership.sql
@@ -1,6 +1,6 @@
 INSERT INTO
-    account.membership (type, rate, rate_period)
+    account.membership (type, rate, rate_period, stripe_plan)
 VALUES
-    ('Monthly Membership', 1.99, 'month'),
-    ('Yearly Membership', 19.99, 'year')
+    ('Monthly Membership', 1.99, 'month', 'monthly_premium'),
+    ('Yearly Membership', 19.99, 'year', 'mycroft_ai_premium_annual_1999')
 ;
