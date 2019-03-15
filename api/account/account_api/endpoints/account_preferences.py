ef921c308bb6efb6011cf3a33218d77a6410a035
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-14 23:50:34 -0500

    allow for a user with no preferences setup

--- api/account/account_api/endpoints/account_preferences.py
+++ api/account/account_api/endpoints/account_preferences.py
@@ -7,32 +7,43 @@ from selene.util.db import get_db_connection
 
 
 class AccountPreferencesEndpoint(SeleneEndpoint):
+    def __init__(self):
+        super(AccountPreferencesEndpoint, self).__init__()
+        self.preferences = None
+
     def get(self):
         self._authenticate()
-        response_data = self._build_response()
+        self._get_preferences()
+        if self.preferences is None:
+            response_data = ''
+            response_code = HTTPStatus.NO_CONTENT
+        else:
+            response_data = self._build_response()
+            response_code = HTTPStatus.OK
 
-        return response_data, HTTPStatus.OK
+        return response_data, response_code
 
-    def _build_response(self):
+    def _get_preferences(self):
         with get_db_connection(self.config['DB_CONNECTION_POOL']) as db:
             preference_repository = PreferenceRepository(db, self.account.id)
-            preferences = preference_repository.get_account_preferences()
+            self.preferences = preference_repository.get_account_preferences()
 
-        response_data = asdict(preferences)
-        if preferences.wake_word is not None:
+    def _build_response(self):
+        response_data = asdict(self.preferences)
+        if self.preferences.wake_word is not None:
             response_data['wake_word'] = dict(
-                id=preferences.wake_word.id,
-                name=preferences.wake_word.wake_word
+                id=self.preferences.wake_word.id,
+                name=self.preferences.wake_word.wake_word
             )
-        if preferences.voice is not None:
+        if self.preferences.voice is not None:
             response_data['voice'] = dict(
-                id=preferences.voice.id,
-                name=preferences.voice.display_name
+                id=self.preferences.voice.id,
+                name=self.preferences.voice.display_name
             )
-        if preferences.geography is not None:
+        if self.preferences.geography is not None:
             response_data['geography'] = dict(
-                id=preferences.geography.id,
-                name=preferences.geography.country
+                id=self.preferences.geography.id,
+                name=self.preferences.geography.country
             )
 
         return response_data
