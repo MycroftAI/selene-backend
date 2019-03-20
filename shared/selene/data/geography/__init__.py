6354ad9dd36c18fc6509f161939fb1f777ce11a7
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-20 17:18:16 -0500

    added some entities to the package api

--- shared/selene/data/geography/__init__.py
+++ shared/selene/data/geography/__init__.py
@@ -1,3 +1,7 @@
+from .entity.city import City
+from .entity.country import Country
+from .entity.region import Region
+from .entity.timezone import Timezone
 from .repository.city import CityRepository
 from .repository.country import CountryRepository
 from .repository.region import RegionRepository
