d7ce0ac3de811f489fe63a9a21326d2832a14fc2
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-20 11:04:13 -0500

    new account_default table repository code

---
+++ shared/selene/data/device/entity/default.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from selene.data.geography import City, Country, Region, Timezone
+from .text_to_speech import TextToSpeech
+from .wake_word import WakeWord
+
+
+@dataclass
+class AccountDefaults(object):
+    id: str
+    city: City = None
+    country: Country = None
+    region: Region = None
+    timezone: Timezone = None
+    voice: TextToSpeech = None
+    wake_word: WakeWord = None
