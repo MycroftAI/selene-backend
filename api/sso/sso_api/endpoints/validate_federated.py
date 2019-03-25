a68424264821ad751b9237b4d50bbb381fde260b
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-25 18:40:01 -0500

    to improve security, UI will pass authentication tokens to this endpoint instead of email addresses.  endpoint will use tokens to obtain email address from provider.

--- api/sso/sso_api/endpoints/validate_federated.py
+++ api/sso/sso_api/endpoints/validate_federated.py
@@ -8,7 +8,9 @@ on our database and build JWTs for access and refresh.
 from http import HTTPStatus
 from logging import getLogger
 
+from facebook import GraphAPI
 from flask import json
+import requests
 
 from selene.api import SeleneEndpoint
 from selene.data.account import AccountRepository, RefreshTokenRepository
@@ -19,8 +21,13 @@ _log = getLogger()
 
 
 class ValidateFederatedEndpoint(SeleneEndpoint):
+    def __init__(self):
+        super(ValidateFederatedEndpoint, self).__init__()
+        self.email_address = None
+
     def post(self):
         """Process a HTTP POST request."""
+        self._get_email_address()
         self._get_account_by_email()
         self._generate_tokens()
         self._set_token_cookies()
@@ -29,16 +36,38 @@ class ValidateFederatedEndpoint(SeleneEndpoint):
 
         return self.response
 
+    def _get_email_address(self):
+        request_data = json.loads(self.request.data)
+        if request_data['platform'] == 'Google':
+            self._get_email_from_google(request_data['token'])
+        elif request_data['platform'] == 'Facebook':
+            self._get_email_from_facebook(request_data['token'])
+
+    def _get_email_from_google(self, token):
+        google_response = requests.get(
+            'https://oauth2.googleapis.com/tokeninfo?id_token=' + token
+        )
+        auth_data = json.loads(google_response.content)
+        self.email_address = auth_data['email']
+
+    def _get_email_from_facebook(self, token):
+        facebook_api = GraphAPI(token)
+        facebook_account = facebook_api.get_object(id='me?fields=email')
+        self.email_address = facebook_account['email']
+
     def _get_account_by_email(self):
         """Use email returned by the authentication platform for validation"""
-        request_data = json.loads(self.request.data)
-        email_address = request_data['email']
+        if self.email_address is None:
+            raise AuthenticationError('could not retrieve email from provider')
+
         with get_db_connection(self.config['DB_CONNECTION_POOL']) as db:
             acct_repository = AccountRepository(db)
-            self.account = acct_repository.get_account_by_email(email_address)
+            self.account = acct_repository.get_account_by_email(
+                self.email_address
+            )
 
         if self.account is None:
-            raise AuthenticationError('account not found')
+            raise AuthenticationError('no account found for provided email')
 
     def _add_refresh_token_to_db(self):
         """Track refresh tokens in the database.
