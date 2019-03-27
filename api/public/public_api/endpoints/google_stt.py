aabc779647b43dcd6f8a0d95f0e826f6c2303f7b
Matheus Lima <matheuslimaufc@gmail.com>
2019-03-27 13:11:38 -0300

    fixing bug with google stt and fixing device's setting to properly return date format and time format

--- api/public/public_api/endpoints/google_stt.py
+++ api/public/public_api/endpoints/google_stt.py
@@ -24,13 +24,16 @@ class GoogleSTTEndpoint(PublicEndpoint):
             response = self.recognizer.recognize_google(data, key=self.google_stt_key, language=lang, show_all=True)
             if isinstance(response, dict):
                 alternative = response.get("alternative")
-                # Sorting by confidence:
-                alternative = sorted(alternative, key=lambda alt: alt['confidence'], reverse=True)
-                alternative = [alt['transcript'] for alt in alternative]
-                # Return n transcripts with the higher confidence. That is useful for the case when send a ambiguous
-                # voice file and the correct utterance is not the utterance with highest confidence and the API client
-                # is interested in test the utterances found.
-                result = alternative if len(alternative) <= limit else alternative[:limit]
+                if 'confidence' in alternative:
+                    # Sorting by confidence:
+                    alternative = sorted(alternative, key=lambda alt: alt['confidence'], reverse=True)
+                    alternative = [alt['transcript'] for alt in alternative]
+                    # Return n transcripts with the higher confidence. That is useful for the case when send a ambiguous
+                    # voice file and the correct utterance is not the utterance with highest confidence and the API
+                    # client is interested in test the utterances found.
+                    result = alternative if len(alternative) <= limit else alternative[:limit]
+                else:
+                    result = [alternative[0]['transcript']]
             else:
                 result = []
         return result
