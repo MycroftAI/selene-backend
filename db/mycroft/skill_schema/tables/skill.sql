55b75e01116aba94d63d2607e58ff9a53fd38323
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-28 14:07:51 -0500

    changed the url column to be a new global id column that is unique

--- db/mycroft/skill_schema/tables/skill.sql
+++ db/mycroft/skill_schema/tables/skill.sql
@@ -1,6 +1,6 @@
 CREATE TABLE skill.skill (
     id          uuid        PRIMARY KEY DEFAULT gen_random_uuid(),
+    global_id   text        NOT NULL UNIQUE,
     name        text        NOT NULL,
-    url         text,
     insert_ts   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
