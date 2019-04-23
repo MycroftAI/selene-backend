c4776c5774ff116d9944dfd2796ea1e612f8fc9a
Chris Veilleux <veilleux.chris@gmail.com>
2019-04-23 12:30:57 -0500

    stubbed in view for oauth checks

---
+++ api/account/account_api/endpoints/skill_oauth.py
@@ -0,0 +1,27 @@
+from http import HTTPStatus
+import os
+
+import requests
+
+from selene.api import SeleneEndpoint
+
+
+class SkillOauthEndpoint(SeleneEndpoint):
+    def __init__(self):
+        super(SkillOauthEndpoint, self).__init__()
+        self.oauth_base_url = os.environ['OAUTH_BASE_URL']
+
+    def get(self, oauth_id):
+        self._authenticate()
+        self._get_oauth_url(oauth_id)
+
+        return '', HTTPStatus.NO_CONTENT
+
+    def _get_oauth_url(self, oauth_id):
+        url = '{base_url}/{oauth_id}/auth_url?uuid={account_id}'.format(
+            base_url=self.oauth_base_url,
+            oauth_id=oauth_id,
+            account_id=self.account.id
+        )
+        response = requests.get(url)
+        pass
