0435b883d05b475a5c2b2547631896047a8f3131
Chris Veilleux <veilleux.chris@gmail.com>
2019-05-23 22:24:57 -0500

    changed account_id to be an instance attribute and added it to the query that updates skill settings

--- shared/selene/data/skill/repository/setting.py
+++ shared/selene/data/skill/repository/setting.py
@@ -8,16 +8,15 @@ from ...repository_base import RepositoryBase
 
 
 class SkillSettingRepository(RepositoryBase):
-    def __init__(self, db):
+    def __init__(self, db, account_id: str):
         super(SkillSettingRepository, self).__init__(db, __file__)
         self.db = db
+        self.account_id = account_id
 
-    def get_account_skill_settings(
-            self, skill_id: str, account_id: str
-    ) -> List[AccountSkillSetting]:
+    def get_skill_settings(self, skill_id: str) -> List[AccountSkillSetting]:
         db_request = self._build_db_request(
             'get_settings_for_skill.sql',
-            args=dict(skill_id=skill_id, account_id=account_id)
+            args=dict(skill_id=skill_id, account_id=self.account_id)
         )
         db_result = self.cursor.select_all(db_request)
 
@@ -34,9 +33,9 @@ class SkillSettingRepository(RepositoryBase):
 
         return skill_settings
 
-    def get_installer_settings(self, account_id: str) -> List[AccountSkillSetting]:
+    def get_installer_settings(self) -> List[AccountSkillSetting]:
         skill_repo = SkillRepository(self.db)
-        skills = skill_repo.get_skills_for_account(account_id)
+        skills = skill_repo.get_skills_for_account(self.account_id)
         installer_skill_id = None
         for skill in skills:
             if skill.display_name == 'Installer':
@@ -44,22 +43,24 @@ class SkillSettingRepository(RepositoryBase):
 
         skill_settings = None
         if installer_skill_id is not None:
-            skill_settings = self.get_account_skill_settings(
-                installer_skill_id,
-                account_id
-            )
+            skill_settings = self.get_skill_settings(installer_skill_id)
 
         return skill_settings
 
     @use_transaction
-    def update_device_skill_settings(self, skill_id, skill_settings):
+    def update_skill_settings(
+            self,
+            skill_id: str,
+            skill_settings: List[AccountSkillSetting]
+    ):
         for settings_group in skill_settings:
             db_request = self._build_db_request(
                 'update_device_skill_settings.sql',
                 args=dict(
-                    settings_values=json.dumps(settings_group['settingsValues']),
+                    account_id=self.account_id,
+                    settings_values=json.dumps(settings_group.settings_values),
                     skill_id=skill_id,
-                    device_names=tuple(settings_group['devices'])
+                    device_names=tuple(settings_group.devices)
                 )
             )
             self.cursor.update(db_request)
