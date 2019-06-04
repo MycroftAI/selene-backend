6a40029dea2ebabfab24e2bd8741f3e76f03bc61
Chris Veilleux <veilleux.chris@gmail.com>
2019-06-04 10:59:41 -0500

    added some comments and docstrings

--- batch/base/base.py
+++ batch/base/base.py
@@ -1,3 +1,9 @@
+"""
+Base class that should be inherited by any batch job.
+
+The logic in this class handles boilerplate code that is required for all batch
+jobs, such as argument parsing, logging setup and database connectivity.
+"""
 from argparse import ArgumentParser
 from datetime import date, datetime
 from logging import getLogger
@@ -33,6 +39,7 @@ class SeleneScript(object):
 
     @property
     def db(self):
+        """Connect to the mycroft database"""
         if self._db is None:
             db_connection_config = DatabaseConnectionConfig(
                 host=environ['DB_HOST'],
@@ -48,6 +55,7 @@ class SeleneScript(object):
         return self._db
 
     def run(self):
+        """Call this method to run the job."""
         try:
             self._start_job()
             self._run()
@@ -59,12 +67,14 @@ class SeleneScript(object):
             self._finish_job()
 
     def _start_job(self):
+        """Initialization tasks."""
         # Logger builds daily files, delineate start in case of multiple runs
         self.log.info('* * * * *  START OF JOB  * * * * *')
         self._define_args()
         self.args = self._arg_parser.parse_args()
 
     def _define_args(self):
+        """Define the command-line arguments for the batch job."""
         self._arg_parser.add_argument(
             "--date",
             default=date.today(),
@@ -77,6 +87,7 @@ class SeleneScript(object):
         raise NotImplementedError
 
     def _finish_job(self):
+        """Tie up any loose ends."""
         self.end_ts = datetime.now()
         self._insert_metrics()
         self.db.close()
@@ -87,6 +98,7 @@ class SeleneScript(object):
         self.log.info('* * * * *  END OF JOB  * * * * *')
 
     def _insert_metrics(self):
+        """Add a row to the job metrics table for monitoring purposes."""
         if self.args is not None:
             job_repository = JobRepository(self.db)
             job_metric = JobMetric(
