de0ca3d1bf64a74c2423518de54efa20fb6d6209
Chris Veilleux <veilleux.chris@gmail.com>
2019-06-12 15:19:50 -0500

    moved api testing library code to the testing library and minor refactor of account tooling

--- shared/selene/testing/__init__.py
+++
@@ -1,5 +0,0 @@
-from .account import add_account, remove_account
-from .account_geography import add_account_geography
-from .agreement import add_agreements, remove_agreements
-from .membership import insert_memberships, delete_memberships
-from .test_db import create_test_db, drop_test_db
--- shared/selene/testing/account.py
+++ shared/selene/testing/account.py
@@ -9,36 +9,14 @@ from selene.data.account import (
 )
 
 
-def build_test_membership(**overrides):
-    return AccountMembership(
-        type=overrides.get('type') or 'Monthly Membership',
-        start_date=overrides.get('start_date') or date.today(),
-        payment_method=overrides.get('payment_method') or 'Stripe',
-        payment_account_id=overrides.get('payment_account_id') or 'foo',
-        payment_id=overrides.get('payment_id') or 'bar'
-    )
-
-
 def build_test_account(**overrides):
     test_agreements = [
         AccountAgreement(type=PRIVACY_POLICY, accept_date=date.today()),
         AccountAgreement(type=TERMS_OF_USE, accept_date=date.today())
     ]
-    if 'membership' in overrides:
-        membership_overrides = overrides['membership']
-        if membership_overrides is None:
-            # if the membership override is set to None, the assumed intent
-            # is to add an account that does not have a membership
-            test_membership = None
-        else:
-            test_membership = build_test_membership(**membership_overrides)
-    else:
-        test_membership = build_test_membership()
-
     return Account(
         email_address=overrides.get('email_address') or 'foo@mycroft.ai',
         username=overrides.get('username') or 'foobar',
-        membership=test_membership,
         agreements=overrides.get('agreements') or test_agreements
     )
 
@@ -46,7 +24,7 @@ def build_test_account(**overrides):
 def add_account(db, **overrides):
     acct_repository = AccountRepository(db)
     account = build_test_account(**overrides)
-    account.id = acct_repository.add(account, 'foo')
+    account.id = acct_repository.add(account, 'test_password')
     if account.membership is not None:
         acct_repository.add_membership(account.id, account.membership)
 
@@ -56,3 +34,22 @@ def add_account(db, **overrides):
 def remove_account(db, account):
     account_repository = AccountRepository(db)
     account_repository.remove(account)
+
+
+def build_test_membership(**overrides):
+    stripe_acct = 'test_stripe_acct_id'
+    return AccountMembership(
+        type=overrides.get('type') or 'Monthly Membership',
+        start_date=overrides.get('start_date') or date.today(),
+        payment_method=overrides.get('payment_method') or 'Stripe',
+        payment_account_id=overrides.get('payment_account_id') or stripe_acct,
+        payment_id=overrides.get('payment_id') or 'test_stripe_payment_id'
+    )
+
+
+def add_account_membership(db, account_id, **overrides):
+    membership = build_test_membership(**overrides)
+    acct_repository = AccountRepository(db)
+    acct_repository.add_membership(account_id, membership)
+
+    return membership
