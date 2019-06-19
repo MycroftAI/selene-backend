f28ec2c502e3b468da98e90709265d7290c90a97
Chris Veilleux <veilleux.chris@gmail.com>
2019-06-19 15:56:31 -0500

    Changed account preferences upsert to use the AccountPreferences object instead of a dictionary

--- shared/selene/data/device/entity/preference.py
+++ shared/selene/data/device/entity/preference.py
@@ -1,18 +1,9 @@
 from dataclasses import dataclass
 
-from .text_to_speech import TextToSpeech
-from .wake_word import WakeWord
-
 
 @dataclass
 class AccountPreferences(object):
-    id: str
     date_format: str
     time_format: str
     measurement_system: str
-    country: str = None
-    city: str = None
-    region: str = None
-    timezone: str = None
-    voice: TextToSpeech = None
-    wake_word: WakeWord = None
+    id: str = None
