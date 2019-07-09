21501d36123457b3882a52af4965932e92f4e3ba
Chris Veilleux <veilleux.chris@gmail.com>
2019-07-08 19:30:25 -0500

    bug fix - the UI was throwing errors when a skill did not have settings.  initialize an empty settings section to fix.

--- api/account/account_api/endpoints/skill_settings.py
+++ api/account/account_api/endpoints/skill_settings.py
@@ -1,3 +1,4 @@
+"""Endpoint to return the skill settings for a given skill family."""
 from http import HTTPStatus
 
 from flask import json, Response
@@ -21,12 +22,14 @@ class SkillSettingsEndpoint(SeleneEndpoint):
 
     @property
     def setting_repository(self):
+        """Only instantiate the SkillSettingsRepository if needed."""
         if self._setting_repository is None:
             self._setting_repository = SkillSettingRepository(self.db)
 
         return self._setting_repository
 
     def get(self, skill_family_name):
+        """Process an HTTP GET request"""
         self._authenticate()
         self.family_settings = self.setting_repository.get_family_settings(
             self.account.id,
@@ -44,6 +47,13 @@ class SkillSettingsEndpoint(SeleneEndpoint):
         )
 
     def _parse_selection_options(self):
+        """Parse the dropdown options string into a list of options.
+
+        Drop-down options are defined in a skill's settingsmeta.json as such:
+            <label 1>|<value 1>;<label 2>|<value 2>;<label 3>|<value 3>...etc
+        Convert this string into a dictionary where the key is the label and
+        the value is the value.
+        """
         for skill_settings in self.family_settings:
             if skill_settings.settings_display is not None:
                 for section in skill_settings.settings_display['sections']:
@@ -61,8 +71,13 @@ class SkillSettingsEndpoint(SeleneEndpoint):
                             field['options'] = parsed_options
 
     def _build_response_data(self):
+        """Build the object to return to the UI."""
         response_data = []
         for skill_settings in self.family_settings:
+            # The UI will throw an error if settings display is null due to how
+            # the skill settings data structures are defined.
+            if skill_settings.settings_display is None:
+                skill_settings.settings_display = dict(sections=[])
             response_skill = dict(
                 settingsDisplay=skill_settings.settings_display,
                 settingsValues=skill_settings.settings_values,
@@ -73,12 +88,14 @@ class SkillSettingsEndpoint(SeleneEndpoint):
         return response_data
 
     def put(self, skill_family_name):
+        """Process a HTTP PUT request"""
         self._authenticate()
         self._update_settings_values()
 
         return '', HTTPStatus.OK
 
     def _update_settings_values(self):
+        """Update the value of the settings column on the device_skill table,"""
         for new_skill_settings in self.request.json['skillSettings']:
             account_skill_settings = AccountSkillSetting(
                 settings_display=new_skill_settings['settingsDisplay'],
