8baa65796941ff2520ce44a89ad9e02f4d416037
Chris Veilleux <veilleux.chris@gmail.com>
2019-09-05 18:18:16 -0500

    replaced an upsert statement with a query because there can be multiple settings definitions for a skill.

--- shared/selene/data/skill/repository/settings_display.py
+++ shared/selene/data/skill/repository/settings_display.py
@@ -10,6 +10,7 @@ class SettingsDisplayRepository(RepositoryBase):
         super(SettingsDisplayRepository, self).__init__(db, __file__)
 
     def add(self, settings_display: SettingsDisplay) -> str:
+        """Add a new row to the skill.settings_display table."""
         db_request = self._build_db_request(
             sql_file_name='add_settings_display.sql',
             args=dict(
@@ -22,6 +23,7 @@ class SettingsDisplayRepository(RepositoryBase):
         return result['id']
 
     def get_settings_display_id(self, settings_display: SettingsDisplay):
+        """Get the ID of a skill's settings definition."""
         db_request = self._build_db_request(
             sql_file_name='get_settings_display_id.sql',
             args=dict(
@@ -33,16 +35,21 @@ class SettingsDisplayRepository(RepositoryBase):
 
         return None if result is None else result['id']
 
-    def upsert(self, skill_id, settings_definition):
-        self._build_db_request(
-            sql_file_name='upsert_setting_definition.sql',
-            args=dict(
-                skill_id=skill_id,
-                settings_definition=settings_definition
-            )
+    def get_settings_definitions_by_gid(self, global_id):
+        """Get all matching settings definitions for a global skill ID.
+
+        There can be more than one settings definition for a global skill ID.
+        An example of when this could happen is if a skill author changed the
+        settings definition and not all devices have updated to the latest.
+        """
+        return self._select_all_into_dataclass(
+            SettingsDisplay,
+            sql_file_name='get_settings_definition_by_gid.sql',
+            args=dict(global_id=global_id)
         )
 
     def remove(self, settings_display_id: str):
+        """Delete a settings definition that is no longer used by any device"""
         db_request = self._build_db_request(
             sql_file_name='delete_settings_display.sql',
             args=dict(settings_display_id=settings_display_id)
