88943afe37dcd37010c119c37a239e57114b6998
Chris Veilleux <veilleux.chris@gmail.com>
2020-09-17 13:56:11 -0500

    add status to wake word file table

--- db/mycroft/tagging_schema/tables/file.sql
+++ db/mycroft/tagging_schema/tables/file.sql
@@ -6,5 +6,6 @@ CREATE TABLE tagging.file (
     submission_date     date                        NOT NULL DEFAULT CURRENT_DATE,
     file_location_id    uuid                        REFERENCES tagging.file_location,
     account_id          uuid,
+    status              tagging_file_status_enum    NOT NULL DEFAULT 'uploaded'::tagging_file_status_enum,
     insert_ts           TIMESTAMP                   NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
