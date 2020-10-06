0106523772f22a8ea0beee2bebf23f50c0ab203e
Chris Veilleux <veilleux.chris@gmail.com>
2020-10-06 00:33:29 -0500

    make the file location on the tagging.file table not nullable

--- db/mycroft/versions/2020.9.1.sql
+++ db/mycroft/versions/2020.9.1.sql
@@ -155,7 +155,7 @@ CREATE TABLE tagging.file (
     name                text                        NOT NULL UNIQUE,
     origin              tagging_file_origin_enum    NOT NULL,
     submission_date     date                        NOT NULL DEFAULT CURRENT_DATE,
-    file_location_id    uuid                        REFERENCES tagging.file_location,
+    file_location_id    uuid                        NOT NULL REFERENCES tagging.file_location,
     account_id          uuid,
     status              tagging_file_status_enum    NOT NULL DEFAULT 'uploaded'::tagging_file_status_enum,
     insert_ts           TIMESTAMP                   NOT NULL DEFAULT CURRENT_TIMESTAMP
