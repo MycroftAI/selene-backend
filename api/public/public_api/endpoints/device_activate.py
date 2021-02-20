2f49f797b1cf85b4a719ac77dd9fff0b261786ba
Chris Veilleux <veilleux.chris@gmail.com>
2021-02-20 17:17:53 -0600

    Change the logic to get the Pantacor device configuration to occur at activation time and use the Pantacor device ID sent to the API by the device.

--- api/public/public_api/endpoints/device_activate.py
+++ api/public/public_api/endpoints/device_activate.py
@@ -32,41 +32,56 @@ from http import HTTPStatus
 from schematics import Model
 from schematics.types import StringType
 
-from selene.api import PublicEndpoint
-from selene.api import generate_device_login
+from selene.api import generate_device_login, PublicEndpoint
+from selene.api.pantacor import get_pantacor_device
 from selene.data.device import DeviceRepository
 from selene.util.cache import DEVICE_PAIRING_TOKEN_KEY
 
 
 class ActivationRequest(Model):
+    """Data model of the fields in the request."""
+
     token = StringType(required=True)
     state = StringType(required=True)
-    platform = StringType(default='unknown')
-    coreVersion = StringType(default='unknown')
-    enclosureVersion = StringType(default='unknown')
+    platform = StringType(default="unknown")
+    core_version = StringType(default="unknown")
+    enclosure_version = StringType(default="unknown")
     platform_build = StringType()
+    pantacor_device_id = StringType()
 
 
 class DeviceActivateEndpoint(PublicEndpoint):
+    """API endpoint for activating a device, the last step in device pairing."""
+
+    _device_repository = None
+
+    @property
+    def device_repository(self):
+        """Lazily load an instance of the device repository"""
+        if self._device_repository is None:
+            self._device_repository = DeviceRepository(self.db)
+
+        return self._device_repository
+
     def post(self):
+        """Process a HTTP POST request."""
         activation_request = self._validate_request()
         pairing_session = self._get_pairing_session()
         if pairing_session is not None:
-            device_id = pairing_session['uuid']
+            device_id = pairing_session["uuid"]
             self._activate(device_id, activation_request)
-            response = (
-                generate_device_login(device_id, self.cache),
-                HTTPStatus.OK
-            )
+            response = (generate_device_login(device_id, self.cache), HTTPStatus.OK)
         else:
-            response = '', HTTPStatus.NOT_FOUND
+            response = "", HTTPStatus.NOT_FOUND
+
         return response
 
-    def _validate_request(self):
+    def _validate_request(self) -> dict:
+        """Validate the contents of the API request against the data model."""
         activation_request = ActivationRequest(self.request.json)
         activation_request.validate()
 
-        return activation_request
+        return activation_request.to_native()
 
     def _get_pairing_session(self):
         """Get the pairing session from the cache.
@@ -74,22 +89,41 @@ class DeviceActivateEndpoint(PublicEndpoint):
         The request must have same state value as that stored in the
         pairing session.
         """
-        pairing_session_token = self.request.json['token']
+        pairing_session_token = self.request.json["token"]
         pairing_session_key = DEVICE_PAIRING_TOKEN_KEY.format(
             pairing_token=pairing_session_token
         )
         pairing_session = self.cache.get(pairing_session_key)
         if pairing_session:
             pairing_session = json.loads(pairing_session)
-            if self.request.json['state'] == pairing_session['state']:
+            if self.request.json["state"] == pairing_session["state"]:
                 self.cache.delete(pairing_session_key)
-                return pairing_session
 
-    def _activate(self, device_id: str, activation_request: ActivationRequest):
-        """Updates the core version, platform and enclosure_version columns"""
+        return pairing_session
+
+    def _activate(self, device_id: str, activation_request: dict):
+        """Updates the core version, platform and enclosure_version columns
+
+        :param device_id: internal identifier of the device
+        :param activation_request: validated request data
+        """
         updates = dict(
-            platform=str(activation_request.platform),
-            enclosure_version=str(activation_request.enclosureVersion),
-            core_version=str(activation_request.coreVersion)
+            platform=str(activation_request["platform"]),
+            enclosure_version=str(activation_request["enclosure_version"]),
+            core_version=str(activation_request["core_version"]),
         )
-        DeviceRepository(self.db).update_device_from_core(device_id, updates)
+        self.device_repository.update_device_from_core(device_id, updates)
+
+        if activation_request["pantacor_device_id"] is not None:
+            self._add_pantacor_config(
+                device_id, activation_request["pantacor_device_id"]
+            )
+
+    def _add_pantacor_config(self, device_id: str, pantacor_device_id: str):
+        """The software updates are managed by Pantacor, get their ID and add to DB
+
+        :param device_id: internal identifier of the device
+        :param pantacor_device_id: identifier of a device on the Pantacor system
+        """
+        pantacor_config = get_pantacor_device(pantacor_device_id)
+        self.device_repository.add_pantacor_config(device_id, pantacor_config)
--- api/public/tests/features/device_pairing.feature
+++ api/public/tests/features/device_pairing.feature
@@ -7,8 +7,17 @@ Feature: Pair a device
     And the pairing data is stored in Redis
     And the pairing data is sent to the device
 
-  Scenario: Device Activation
+  Scenario: Device activation
     Given the user completes the pairing process on the web application
     When the device requests to be activated
     Then the request will be successful
     And the activation data is sent to the device
+    And the device attributes are stored in the database
+
+  Scenario: Pantacor device activation
+    Given the user completes the pairing process on the web application
+    When a device using Pantacor requests to be activated
+    Then the request will be successful
+    And the activation data is sent to the device
+    And the device attributes are stored in the database
+    And the Pantacor device configuration is stored in the database
